7ecdb5ce025e9016998731449eca1b68ccdf7162
marky376 <mmutuamark@gmail.com>
2024-10-26 19:28:03 +0300

    A context switching animation

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import ContextSwitch from "./elements/ContextSwitch/ContextSwitch";
+import "./styles.css";
+
+export default function App() {
+  const [darkMode, setDarkMode] = React.useState(true);
+
+  document.documentElement.classList.toggle("dark", darkMode);
+
+  const handleChange = React.useCallback<(isOn: boolean) => void>(
+    (isOn) => {
+      setDarkMode(isOn);
+      document.documentElement.classList.toggle("dark", isOn);
+    },
+    [darkMode]
+  );
+
+  return (
+    <div className="App">
+      <img
+        className="img"
+        src="https://pbs.twimg.com/media/GaHNPI1WUAANFRC?format=jpg&name=4096x4096"
+      />
+      <ContextSwitch on={!darkMode} onChange={handleChange} />
+    </div>
+  );
+}
